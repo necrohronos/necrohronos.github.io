303fafcd89c75dfe291e1a2252da5c3a7c96817b
Bogdan Ciechomski <bogdan.ciechomski@gmail.com>
2018-07-19 15:33:36 +0200

    typy skończone

--- warsztaty/javascript/js/typy.js
+++ warsztaty/javascript/js/typy.js
@@ -1,4 +1,13 @@
 //sprawdzanie typu
 let liczba = 10;
-let napis = 'ala'
+let napis = `Ala ma ${liczba} lat`;
+let napis2 = 'Ala ma ' + liczba + ' lat';
 console.log (typeof napis);
+console.log (napis);
+console.log (napis2);
+
+//wartości logiczne booleanowskie true false
+if(true) {
+    console.log('prawda')
+}
+
