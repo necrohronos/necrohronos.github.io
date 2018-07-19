677ee9819a67426d0a91f59f3fcfd7664100c1a7
Bogdan Ciechomski <bogdan.ciechomski@gmail.com>
2018-07-19 13:38:53 +0200

    wstęp do js

---
+++ warsztaty/javascript/js/main.js
@@ -0,0 +1,16 @@
+//komentarz
+
+/*
+rerw
+Reflect
+falsee
+require
+*/
+
+//zmiene
+let gfg;
+gfg = 10;
+//stałe
+const numberOfMonths = 12;
+console.log(gfg)
+//camelCase to pisanie nazwy zmiennej od malej, apotem dużymi
