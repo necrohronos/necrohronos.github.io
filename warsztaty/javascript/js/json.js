c8970d9e3804b6aa56111ab2417b08d36ae71c8c
Bogdan Ciechomski <bogdan.ciechomski@gmail.com>
2018-07-20 11:26:52 +0200

    DOM start

--- warsztaty/javascript/js/json.js
+++ warsztaty/javascript/js/json.js
@@ -1,4 +1,4 @@
-let jsonObj = {
+/*let jsonObj = {
     'employees':[
         {'firstName': 'John', 'lastName': 'Doe'}, 
         {'firstName': 'Anna','lastName': 'Smith'}, 
@@ -8,11 +8,16 @@ let jsonObj = {
 /*
 console.log(jsonObj.employees[1].lastName); */
 
-for(let i = 0; i < jsonObj.employees.length; i++) {
+/* for(let i = 0; i < jsonObj.employees.length; i++) {
     console.log(jsonObj.employees[i].firstName + ' ' + jsonObj.employees[i].lastName);
 };
 
 
 jsonObj.employees.forEach(function(element, index) {
     console.log(element.firstName + ' ' + element.lastName);
-})
+}) */
+
+function iloczyn(x,y,z) {
+    return x * y * z;
+}
+console.log(iloczyn(2,4,6));
