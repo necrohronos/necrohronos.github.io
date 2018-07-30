7fd750553fc2ff9ccbf4172642a683685c24ac87
Bogdan Ciechomski <bogdan.ciechomski@gmail.com>
2018-07-30 11:21:50 +0200

    bootstrap

---
+++ warsztaty/bootstrap/js/boot.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('#carouselExampleIndicators').carousel({
+        "interval": 10,
+        "pause": false,
+    });
+})
--- zadania/kalkulator/js/calc.js
+++ zadania/kalkulator/js/calc.js
@@ -1,28 +1,37 @@
-//document.addEventListener('DOMContentLoaded', function() {
-/*	let noButton = document.getElementsByClassName('number');
-	let ekranik = document.getElementById('podglad');
-	let resultButton = document.getElementById('wynik');
-  let clearButton = document.getElementById('delete'); */
-function widok() { 
-        //$('#podglad').html += $('.number').value;};
-        console.log($('.numbers').value)}
-$(document).ready(function() {
-	$('.number').click(widok) 
-    $('#wynik').click(function(){var wynik = eval(widok()); $('#podglad').html(wynik)})
-    $('#delete').click(function() {$('#podglad').empty()})
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    let noButtons = document.getElementsByClassName('number');
+    let ekranik = document.getElementById('podglad');
+    let resultButton = document.getElementById('wynik');
+    let clearButton = document.getElementById('delete');
+
+//    console.log(noButton);
+    
+    function widok() {
+        ekranik.innerHTML += noButton.innerText;
+        console.log(noButton.innerText);
+    }
+//pętla rozdzielająca wszystkie elementy na pojedyńcze
+    for(noButton of noButtons) {
+        noButton.addEventListener('click', widok);
+//        console.log(noButton);
+    }
+    
+//    noButton.addEventListener('click', widok);
+    resultButton.addEventListener('click', function () {
+        let wynik = eval(widok());
+        ekranik.innerHTML(wynik);
+    })
+    clearButton.addEventListener('click', function () {
+        ekranik.innerHTML('');
+    })
 });
-    
-    
-   /* function widok() {
-  ekranik.innerHTML += this.value;
-  console.log(this.value);
-  }
-  document.getElementsByClassName('number').addEventListener('click', widok());
-  resultButton.addEventListener('click', function() {
-    let wynik = eval(widok());
-    ekranik.innerHTML(wynik);
-  })
-  clearButton.addEventListener('click', function() {
-    ekranik.innerHTML('');
-  }) 
-}); */
+/*
+1. przyciski z jedna klasą
+1a div do wyświetlania 
+2. addEventListener podpiąć pod buttony
+dla cyfr i znaków funkcja pobierająca value i wyświetla;
+3. delete
+4. funkcja wykonyjąca działanie dla wartości w wyświetlaczu eval()
+2. petla dla przycisków */
